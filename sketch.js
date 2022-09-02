6cc18f6f8deb3f715e391b721b7d699bb2f042fa
Jacob McMichael <jacobmcmichael@gmail.com>
2022-09-02 16:44:58 -0700

    Draw vertices and lines
    Signed-off-by: Jacob McMichael <jacobmcmichael@gmail.com>

--- sketch.js
+++ sketch.js
@@ -6,29 +6,54 @@ function preload() {
 let points;
 let bounds;
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(1000, 1000);
     stroke(0);
-    fill(255, 104, 204);
+    fill(0, 0, 0);
 
-    points = font.textToPoints('hello world', 0, 0, 20, {
-        sampleFactor: 50,
+    points = font.textToPoints('A', 0, 0, 600, {
+        sampleFactor: 0.1,
         simplifyThreshold: 0
     });
-    bounds = font.textBounds(' hello world ', 0, 0, 20);
+    bounds = font.textBounds(' A ', 0, 0, 600);
+
+    function shuffle(arr) {
+        var j, x, i;
+        for (i = arr.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = arr[i];
+            arr[i] = arr[j];
+            arr[j] = x;
+        }
+        return arr;
+    }
+
+    shuffle(points);
+
+    console.log(points[0]);
 }
+// line(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y)
 
 function draw() {
-    background(255);
-    beginShape();
-    translate(-bounds.x * width / bounds.w, -bounds.y * height / bounds.h);
+    background(225, 225, 225);
+
+    // draw vertices
+    beginShape(POINTS);
+    stroke(0, 0, 0);
+    strokeWeight(5);
+
+    translate((width / 2) - 300, (height / 2) + 300);
 
     for (let i = 0; i < points.length; i++) {
-        let p = points[i];
-        vertex(
-            p.x * width / bounds.w +
-            sin(20 * p.y / bounds.h + millis() / 1000) * width / 30,
-            p.y * height / bounds.h
-        );
+        vertex(points[i].x, points[i].y);
     }
-    endShape(CLOSE);
+    endShape();
+
+    // draw lines
+    beginShape();
+    stroke(100, 100, 100);
+    strokeWeight(1);
+    for (let i = 0; i < points.length; i++) {
+        line(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y)
+    }
+    endShape();
 }
